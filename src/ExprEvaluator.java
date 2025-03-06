49728e08371b666929557a8061ce2b79e3ad72f6
Hemapriya2003 <98634085+Hemapriya2003@users.noreply.github.com>
2025-03-06 00:41:35 -0500

    Update ExprEvaluator.java

--- src/ExprEvaluator.java
+++ src/ExprEvaluator.java
@@ -2,6 +2,30 @@ package src;
 
 public class ExprEvaluator implements ExprVisitor<Float> {
     /*
-     * Part 2: impelement the visitor pattern for expressions
+     * Part 2: implement the visitor pattern for expressions
      */
+    @Override
+    public Float visit(PlusExpr plusExpr) {
+        return plusExpr.getE1().accept(this) + plusExpr.getE2().accept(this);
+    }
+
+    @Override
+    public Float visit(MinusExpr minusExpr) {
+        return minusExpr.getE1().accept(this) - minusExpr.getE2().accept(this);
+    }
+
+    @Override
+    public Float visit(TimesExpr timesExpr) {
+        return timesExpr.getE1().accept(this) * timesExpr.getE2().accept(this);
+    }
+
+    @Override
+    public Float visit(DivExpr divExpr) {
+        return divExpr.getE1().accept(this) / divExpr.getE2().accept(this);
+    }
+
+    @Override
+    public Float visit(FloatExpr floatExpr) {
+        return floatExpr.literal;
+    }
 }
