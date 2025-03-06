ac9dbe6a583a81114358e2d7f955e9b631b81b0d
Hemapriya2003 <98634085+Hemapriya2003@users.noreply.github.com>
2025-03-06 00:41:57 -0500

    Update ExprVisitor.java

--- src/ExprVisitor.java
+++ src/ExprVisitor.java
@@ -4,4 +4,9 @@ public interface ExprVisitor<R> {
     /*
      * Part 2: Implement the visitor pattern for expressions
      */
+    R visit(PlusExpr plusExpr);
+    R visit(MinusExpr minusExpr);
+    R visit(TimesExpr timesExpr);
+    R visit(DivExpr divExpr);
+    R visit(FloatExpr floatExpr);
 }
